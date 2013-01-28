e240a8b4cc861c05dd7fdeee28dda4ab6c0236ed
fukayatsu <fukayatsu@gmail.com>
2013-01-29 00:54:01 +0900

    add TODO comment

--- julius/client.rb
+++ julius/client.rb
@@ -28,6 +28,7 @@ while true
   when '[電気付けて]'
     iremocon.is 1
   when '[消灯]'
+    # TODO: 明るさセンサーを組み込む
     3.times do
       iremocon.is 1
       sleep 0.5
@@ -39,6 +40,18 @@ while true
   when '[今何時？]'
     # macos lionでKyokoをインストールしておくこと
     `say -v Kyoko #{Time.now.strftime('%H:%M')}です。`
+  when '[おはよう]'
+    # `say -v Kyoko おはようございます` #=> 無限ループ
+    # TODO: 日次・曜日の読み上げ、天気情報
+  when '[おやすみ]'
+    # `say -v Kyoko おやすみなさい` #=> 無限ループ
+    # TODO: エアコン、照明を消す
+  when '[ただいま]'
+    `say -v Kyoko おかえりなさい`
+    # 照明ON、温度によってはエアコンを付ける
+  when '[行ってきます]'
+    # `say -v Kyoko いってらっしゃい` #=> 無限ループ
+    # 照明OFF、エアコンOFF
   end
 
 end
