c5097cba10ed69dbc4364967798606f46f673ad7
fukayatsu <fukayatsu@gmail.com>
2013-02-01 02:02:01 +0900

    add song

--- roomba/roomba.rb
+++ roomba/roomba.rb
@@ -1,10 +1,15 @@
 require 'serialport'
 
 class Roomba
+  attr_accessor :serial_port
   def initialize
     @serial_port = SerialPort.new('/dev/tty.FireFly-86A2-SPP', 115200, 8, 1, 0)
+    # @serial_port.read_timeout = 100
 
-    write([128, 130])
+    sleep 0.5
+    write(128)
+    sleep 0.2
+    write(130)
   end
 
   def exit
@@ -22,12 +27,24 @@ class Roomba
       return true
     end
 
+    p args
     @serial_port.write(args.chr)
-    sleep 0.2
+  end
+
+  def read
+    data = []
+    until (b = @serial_port.getbyte).nil?
+      p data.push b
+    end
+
+    data
   end
 end
 
 
 roomba = Roomba.new
-roomba.write(135)
-roomba.exit
+
+roomba.write [140, 0,9, 67,42, 67,42, 67,42, 63,30, 70,12, 67,40, 63,30, 70,12, 67,20]
+roomba.write [141, 0]
+
+sleep
