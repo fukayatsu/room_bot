99644473cf1463b63f47bef5648c704639e098cb
fukayatsu <fukayatsu@gmail.com>
2013-02-03 22:10:38 +0900

    rescue Errno::EPIPE

--- julius/client.rb
+++ julius/client.rb
@@ -26,38 +26,43 @@ while true
 
   puts command = commands[0]
 
-  case(command)
-  when '[電気付けて]'
-    next if light_status == :on
-    iremocon.is 1
-    light_status = :on
-  when '[電気消して]', '[消灯]'
-    # TODO: 明るさセンサーを組み込む
-    next if light_status == :off
-    3.times do
+  begin
+    case(command)
+    when '[電気付けて]', '[点灯]'
+      next if light_status == :on
       iremocon.is 1
-      sleep 1
+      light_status = :on
+    when '[電気消して]', '[消灯]'
+      # TODO: 明るさセンサーを組み込む
+      next if light_status == :off
+      3.times do
+        iremocon.is 1
+        sleep 1
+      end
+      light_status = :off
+    when '[エアコン付けて]'
+      iremocon.is 2
+    when '[エアコン止めて]', '[エアコン消して]'
+      iremocon.is 3
+    when '[今何時？]'
+      # macos lionでKyokoをインストールしておくこと
+      `say -v Kyoko #{Time.now.strftime('%H:%M')}です。`
+    when '[おはよう]'
+      # `say -v Kyoko おはようございます` #=> 無限ループ
+      # TODO: 日次・曜日の読み上げ、天気情報
+    when '[おやすみ]'
+      # `say -v Kyoko おやすみなさい` #=> 無限ループ
+      # TODO: エアコン、照明を消す
+    when '[ただいま]'
+      `say -v Kyoko おかえりなさい`
+      # 照明ON、温度によってはエアコンを付ける
+    when '[行ってきます]'
+      # `say -v Kyoko いってらっしゃい` #=> 無限ループ
+      # 照明OFF、エアコンOFF
     end
-    light_status = :off
-  when '[エアコン付けて]'
-    iremocon.is 2
-  when '[エアコン止めて]', '[エアコン消して]'
-    iremocon.is 3
-  when '[今何時？]'
-    # macos lionでKyokoをインストールしておくこと
-    `say -v Kyoko #{Time.now.strftime('%H:%M')}です。`
-  when '[おはよう]'
-    # `say -v Kyoko おはようございます` #=> 無限ループ
-    # TODO: 日次・曜日の読み上げ、天気情報
-  when '[おやすみ]'
-    # `say -v Kyoko おやすみなさい` #=> 無限ループ
-    # TODO: エアコン、照明を消す
-  when '[ただいま]'
-    `say -v Kyoko おかえりなさい`
-    # 照明ON、温度によってはエアコンを付ける
-  when '[行ってきます]'
-    # `say -v Kyoko いってらっしゃい` #=> 無限ループ
-    # 照明OFF、エアコンOFF
+  rescue Errno::EPIPE
+    iremocon = Iremocon.new '192.168.11.28'
+    sleep 1
   end
 
 end
